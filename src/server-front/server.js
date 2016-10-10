c063274dda8b097b9e97deedf1b0ef6da8922190
djavrell <kevin.prouteau@epitech.eu>
2016-10-10 17:20:54 +0800

    update server

--- src/server-front/server.js
+++ src/server-front/server.js
@@ -1,6 +1,31 @@
 var express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.get('/data', (req, res) => {
+  console.log('caught a get on /data');
+  res.send({
+    'name': 'light',
+    'data': {
+        label: 'light',
+        data: [
+            100,
+            90,
+            80,
+            70,
+            60,
+            50,
+            40
+        ]
+    }
+  });
+});
+
 app.get('/', (req, res) => {
   res.send('Hello from express');
 });
